0b70afcdf6674e47c64e3c5d3719d0f5f20e4496
a. chkifa <bchkifa@gmail.com>
2023-10-18 17:19:16 +0100

    Update SLE_alias.h
    commented SIMPLEX_MINIMIZE/SIMPLEX_MAXIMIZE

--- SLE_alias.h
+++ SLE_alias.h
@@ -15,8 +15,8 @@ using vec_i = std::vector <int>;
 using vec_d = std::vector <double>;
 using mat_d = std::vector <vec_d>;
 
-#define SIMPLEX_MINIMIZE 1 // not used.
-#define SIMPLEX_MAXIMIZE 2 // not used.
+#define SIMPLEX_MINIMIZE 1 // used for the simplex solver.
+#define SIMPLEX_MAXIMIZE 2 // used for the simplex solver.
 
 
 #endif /* alias_h */
